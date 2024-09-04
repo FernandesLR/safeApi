822275768b293f6e82b1bcebd67382ba9ba296a7
FernandesLr <leo1ramosf@gmail.com>
2024-09-04 14:48:59 -0300

    First Commit

---
+++ src/main/java/com/safeapi/safeapi/SafeapiApplication.java
@@ -0,0 +1,13 @@
+package com.safeapi.safeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safeapi/safeapi/SafeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safeapi.safeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
